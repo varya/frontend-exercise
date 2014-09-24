0d7983ef3d8a57faf603b94ddff6254fb6697f1f
Varya Stepanova <mail@varya.me>
2014-09-24 18:47:23 +0300

    Autoupdate data in ratings table

--- static/js/standings.js
+++ static/js/standings.js
@@ -6,22 +6,7 @@
         return {
             restrict: 'E',
             templateUrl: '/static/templates/standings-table.html',
-            controller: ['$filter', '$http', function($filter, $http) {
-                var _that = this;
-                this.ratings = [];
-
-                var orderBy = $filter('orderBy');
-                this.order = function(predicate, reverse) {
-                    _that.ratings = orderBy(_that.ratings, predicate, reverse);
-                };
-
-                $http
-                    .get('/api/standings.json')
-                    .success(function(data) {
-                        _that.ratings = data;
-                        _that.order('points', true);
-                    });
-            }],
+            controller: 'StandingsController',
             controllerAs: 'standingsCtrl'
         };
     });
@@ -30,19 +15,49 @@
         return {
             restrict: 'E',
             templateUrl: '/static/templates/team-info.html',
-            controller: ['$routeParams', '$http', function($routeParams, $http) {
-                var _that = this;
-                var teamId = $routeParams.teamId;
-                this.team = {};
-
-                $http
-                    .get('/api/team/' + teamId + '.json')
-                    .success(function(data) {
-                        _that.team = data[0];
-                    });
-            }],
+            controller: 'TeamController',
             controllerAs: 'teamCtrl'
         };
     });
+
+    app.controller("StandingsController", ['$scope', '$filter', '$http', '$interval', function($scope, $filter, $http, $interval) {
+        var _that = this;
+
+        this.ratings = [];
+
+        var orderBy = $filter('orderBy');
+
+        this.order = function(predicate, reverse) {
+            _that.ratings = orderBy(_that.ratings, predicate, reverse);
+        };
+
+        this.getData = function() {
+            $http
+                .get('/api/standings.json?rnd=' + Math.random())
+                .success(function(data) {
+                    _that.ratings = data;
+                    _that.order('points', true);
+                });
+        };
+
+        var intervalPromise = $interval(this.getData, 2000);
+        $scope.$on('$destroy', function() {
+            $interval.cancel(intervalPromise);
+        });
+
+        this.getData();
+    }]);
+
+    app.controller('TeamController', ['$routeParams', '$http', function($routeParams, $http) {
+        var _that = this;
+        var teamId = $routeParams.teamId;
+        this.team = {};
+
+        $http
+            .get('/api/team/' + teamId + '.json')
+            .success(function(data) {
+                _that.team = data[0];
+            });
+    }]);
 
 })();
