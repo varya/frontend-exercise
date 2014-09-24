a020eacb3c01c88888b5cdb1d01f815ba08ee5c3
Varya Stepanova <mail@varya.me>
2014-09-24 17:17:44 +0300

    Sorting by points in reatings table

--- static/js/standings.js
+++ static/js/standings.js
@@ -6,14 +6,20 @@
         return {
             restrict: 'E',
             templateUrl: '/static/templates/standings-table.html',
-            controller: ['$http', function($http) {
+            controller: ['$filter', '$http', function($filter, $http) {
                 var _that = this;
                 this.ratings = [];
+
+                var orderBy = $filter('orderBy');
+                this.order = function(predicate, reverse) {
+                    _that.ratings = orderBy(_that.ratings, predicate, reverse);
+                };
 
                 $http
                     .get('/api/standings.json')
                     .success(function(data) {
                         _that.ratings = data;
+                        _that.order('points', true);
                     });
             }],
             controllerAs: 'standingsCtrl'
